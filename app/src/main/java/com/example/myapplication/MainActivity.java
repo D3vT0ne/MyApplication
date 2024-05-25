1c4679b3ffbc76306b9a2da29654c3dc206020d7
D3vT0ne <bogdannabokin17@gmail.com>
2024-05-25 23:12:27 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        final TextView textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = "Ваше_Имя";
+                String surname = "Ваша_Фамилия";
+                textView.setText("Имя: " + name + "\nФамилия: " + surname);
+            }
+        });
+    }
+}
